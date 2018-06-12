c0c0fb04da30c6c5447c182ef8fc30723d44593b
andriy101 <andriy101@gmail.com>
2018-06-12 23:57:31 +0300

    update prime ng version

--- src/utils/lib-versions.js
+++ src/utils/lib-versions.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.angularVersion = '^6.0.0';
-exports.primengVersion = '^6.0.0';
+exports.primengVersion = '^6.0.0-beta.1';
 exports.fontAwesomeVersion = '^4.7.0';
 exports.rxjsCompatVersion = '^6.2.0';
 //# sourceMappingURL=lib-versions.js.map
