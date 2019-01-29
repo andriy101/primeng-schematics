a75decde02545ca770eaa54c132bfd2bc4b85a27
andriy101 <andriy101@gmail.com>
2019-01-29 02:25:06 +0200

    removing JS files

--- src/utils/versions.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.angularVersion = '~7.2.3';
-exports.primengVersion = '~7.0.5';
-exports.primeiconsVersion = '~1.0.0';
-//# sourceMappingURL=versions.js.map
