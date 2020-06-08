f0d5324f924b62fc06c4beda6d005456f3e1fb17
andriy101 <andriy101@gmail.com>
2020-06-09 02:01:54 +0300

    adding version param

--- bin/run.sh
+++ bin/run.sh
@@ -10,23 +10,36 @@ no_angular_app_warning () {
 
 commands="new generate g theme add"
 if [[ $commands =~ (^|[[:space:]])$1($|[[:space:]]) ]] && [ $2 ] || [ $1 = "theme" ] ; then
-  ARGS="$*"
+  ARGS=()
 
   if  [ "$1" = "new" ] ; then
     if  [ "$2" = "help" ] || [ "$2" = "--help" ] || [ "$2" = "-h" ] ; then
       png-utils help new
     else
+      # try to fetch version number from the args (for example: v=8)
+      VERSION=latest
+      for ARG in "$@"; do
+        if [[ "$ARG" =~ ^v=* ]]; then
+          VERSION="${ARG: -1}.x.x"
+        else
+          ARGS+=" $ARG"
+        fi
+      done
+
       if [[ "$OSTYPE" == "msys" ]] ; then
-        npx -p primeng-schematics@7 -p @angular/cli@7 ng $ARGS -c primeng-schematics
+        npx -p primeng-schematics@$VERSION -p @angular/cli@$VERSION ng $ARGS -c primeng-schematics
       else 
-        npx -p primeng-schematics@7 -p @angular/cli@7 -c "ng $ARGS -c primeng-schematics"
+        echo 
+        `npm bin`/png-utils figlet "version: $VERSION"
+
+        # npx -p primeng-schematics@$VERSION -p @angular/cli@$VERSION -c "ng $ARGS -c primeng-schematics"
       fi
 
       # cd to the newly created app and start the app
       cd $2
-      echo
+      echo 
       `npm bin`/png-utils figlet 'ng serve' 
-      echo
+      echo 
       `npm bin`/ng serve -o
     fi
   elif [ "$1" = "g" ] || [ "$1" = "generate" ] ; then
